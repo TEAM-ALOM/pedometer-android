69eb586346ebd434daefef513e5a2d846b684453
bob4587 <bob4587@naver.com>
2023-07-16 12:49:01 +0900

    :lipstick: Main 화면 현재 걸음 및 평균 걸음 UI 추가

--- app/src/main/java/com/example/pedometer/MainActivity.kt
+++ app/src/main/java/com/example/pedometer/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pedometer
 
-import androidx.appcompat.app.AppCompatActivity
+import BaseActivity
 import android.os.Bundle
+import com.example.pedometer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :  BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it)}) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
+        val textStepsToday=binding.viewStepsToday
+        val textStepsAvg=binding.viewStepsAvg
+        textStepsToday.setText("현재 5000 걸음")
+        textStepsAvg.setText("일주일간 평균 8000걸음을 걸었습니다.")
+        //setFragment()
     }
+    /*private fun setFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView, Day())
+        transaction.commit()
+    }*/
 }
