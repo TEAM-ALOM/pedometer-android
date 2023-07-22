5a24323a47c79547d91cd8eaaa2ef249b84c2062
bob4587 <bob4587@naver.com>
2023-07-22 19:01:12 +0900

    :art: 헬스 커넥트 연동(권한 설정 화면 띄우기, 걸음 수 업데이트하기)->아직 걸음 수 데이터 수집 비정상

--- app/src/main/java/com/example/pedometer/MainActivity.kt
+++ app/src/main/java/com/example/pedometer/MainActivity.kt
@@ -2,8 +2,21 @@ package com.example.pedometer
 
 import BaseActivity
 import Day
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.AggregateRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import androidx.lifecycle.lifecycleScope
 import com.example.pedometer.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+import java.time.Instant
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }) {
@@ -32,5 +45,104 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
             .add(R.id.frameLayout, Day())
             .commit()
 
+        val providerPackageName="com.google.android.apps.healthdata"
+        val availabilityStatus = HealthConnectClient.getSdkStatus(this, providerPackageName)
+        if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE) {
+            return // early return as there is no viable integration
+        }
+        if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
+            // Optionally redirect to package installer to find a provider, for example:
+            val uriString = "market://details?id=$providerPackageName&url=https://play.google.com/store/apps/details?id=com.google.android.apps.healthdata&hl=en-KR"
+            this.startActivity(
+                Intent(Intent.ACTION_VIEW).apply {
+                    setPackage("com.android.vending")
+                    data = Uri.parse(uriString)
+                    putExtra("overlay", true)
+                    putExtra("callerId", this@MainActivity.packageName)
+                }
+            )
+            return
+        }
+        val healthConnectClient = HealthConnectClient.getOrCreate(this)
+// Issue operations with healthConnectClient
+
+
+
+    }
+    val PERMISSIONS =
+        setOf(
+            HealthPermission.getReadPermission(HeartRateRecord::class),
+            HealthPermission.getWritePermission(HeartRateRecord::class),
+            HealthPermission.getReadPermission(StepsRecord::class),
+            HealthPermission.getWritePermission(StepsRecord::class)
+        )
+    val requestPermissionActivityContract = PermissionController.createRequestPermissionResultContract()
+
+    val requestPermissions = registerForActivityResult(requestPermissionActivityContract) { granted ->
+        if (granted.containsAll(PERMISSIONS)) {
+            // Permissions successfully granted
+        } else {
+            // Lack of required permissions
+        }
+    }
+
+    suspend fun checkPermissionsAndRun(healthConnectClient: HealthConnectClient) {
+        val granted = healthConnectClient.permissionController.getGrantedPermissions()
+        if (granted.containsAll(PERMISSIONS)) {
+            // Permissions already granted; proceed with inserting or reading data
+        } else {
+            requestPermissions.launch(PERMISSIONS)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        // Health Connect SDK를 사용하여 주기적으로 stepCount 정보를 업데이트
+        updateStepsNow()
+    }
+
+    private fun updateStepsNow() {
+        // 걸음 수 데이터를 얻기 위해 읽기 권한 설정
+        val permissions = setOf(HealthPermission.getReadPermission(StepsRecord::class))
+
+        // Health Connect Client 생성
+        val healthConnectClient = HealthConnectClient.getOrCreate(this)
+
+        // 허용된 권한 확인
+        lifecycleScope.launch {
+            val granted = healthConnectClient.permissionController.getGrantedPermissions()
+            if (granted.containsAll(permissions)) {
+                // 걸음 수 데이터 읽기
+                readStepsData(healthConnectClient)
+            } else {
+                // 권한 요청
+                requestPermissions.launch(permissions)
+            }
+        }
+    }
+    private suspend fun readStepsData(healthConnectClient: HealthConnectClient) {
+        // 현재 시간을 가져와서 endTime으로 설정
+        val endTime = Instant.now()
+        // 24시간 전의 시간을 가져와서 startTime으로 설정
+        val startTime = endTime.minusSeconds(24 * 60 * 60)
+
+        try {
+            // 걸음 수 데이터 읽기
+            val response = healthConnectClient.aggregate(
+                AggregateRequest(
+                    metrics = setOf(StepsRecord.COUNT_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                )
+            )
+            // The result may be null if no data is available in the time range
+            val stepCount = response[StepsRecord.COUNT_TOTAL]
+            stepsNow = (stepCount ?: 0) as Int
+            // 업데이트 된 stepsNow를 화면에 표시
+            textStepsToday.text = "현재 $stepsNow 걸음"
+        } catch (e: Exception) {
+            // 걸음 수 데이터 읽기 실패 시 에러 처리
+            e.printStackTrace()
+            // 또는 다른 방식으로 로그 출력
+            Log.e("MainActivity", "걸음 수 데이터 읽기 실패: ${e.message}")
+        }
     }
 }
