eca3b10988ce8fbe9819176bdfd093fa7f0c69cd
박혜윤 <eunnypark@naver.com>
2023-08-23 15:22:14 +0900

    :lipstick: 캘린더 색깔 추가 (StepViewModel 오류)

--- app/src/main/java/com/example/pedometer/Model/StepViewModel.kt
+++ app/src/main/java/com/example/pedometer/Model/StepViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.pedometer.Model
+import android.graphics.Color
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.applandeo.materialcalendarview.CalendarView
+import com.applandeo.materialcalendarview.EventDay
 import com.example.pedometer.repository.StepRepository
 import kotlinx.coroutines.launch
 
@@ -33,6 +36,28 @@ class StepViewModel(private val stepRepository: StepRepository) : ViewModel() {/
     fun updateStepsGoal(stepsGoal : Int) {
         viewModelScope.launch {
             _stepsGoal.value = stepsGoal ?: 0
+        }
+    }
+
+    // Room Database에서 걸음수와 목표걸음수 가져와서 아이콘 색상을 계산하여 LiveData를 업데이트
+    suspend fun updateCalendarIcons(calendar: CalendarView) {
+        val stepsToday = stepRepository.getStepsToday().value ?: 0
+        val stepsGoal = stepRepository.getStepsGoal().value ?: 0
+
+        val iconColor = calculateIconColor(stepsToday, stepsGoal)
+
+        val eventDay = EventDay(calendar, iconColor) // EventDay 생성
+
+    }
+
+    // 걸음수/목표걸음수의 비율에 따라 아이콘 색상을 계산
+    private fun calculateIconColor(stepsToday: Int, stepsGoal: Int): Int {
+        val ratio = stepsToday.toFloat() / stepsGoal.toFloat()
+
+        return when {
+            ratio >= 1.0 -> Color.BLUE // 파란색 아이콘
+            ratio >= 0.5 -> Color.YELLOW // 노란색 아이콘
+            else -> Color.RED // 빨간색 아이콘
         }
     }
 
